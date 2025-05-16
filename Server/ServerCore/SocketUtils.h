eb7bb63645feb8fc0edb5ac1e29d6be484a2a9d7
DGboost <iuboost8@gmail.com>
2025-05-16 23:33:14 +0900

    Initial commit: 프로젝트를 https://github.com/DGboost/GameServer 와 연동

---
+++ Server/ServerCore/SocketUtils.h
@@ -0,0 +1,43 @@
+#pragma once
+
+// mswsock.h가 이미 포함되어 있어야 하지만 순환 참조 문제를 피하기 위해 
+// 필요한 타입 선언을 직접 전방 선언합니다.
+#include <WinSock2.h>
+#include <MSWSock.h>
+
+/*----------------
+	SocketUtils
+-----------------*/
+
+class SocketUtils
+{
+public:
+	static LPFN_CONNECTEX		ConnectEx;
+	static LPFN_DISCONNECTEX	DisconnectEx;
+	static LPFN_ACCEPTEX		AcceptEx;
+
+public:
+	static void Init();
+	static void Clear();
+
+	static bool BindWindowsFunction(SOCKET socket, GUID guid, LPVOID* fn);
+	static SOCKET CreateSocket();
+
+	static bool SetLinger(SOCKET socket, uint16 onoff, uint16 linger);
+	static bool SetReuseAddress(SOCKET socket, bool flag);
+	static bool SetRecvBufferSize(SOCKET socket, int32 size);
+	static bool SetSendBufferSize(SOCKET socket, int32 size);
+	static bool SetTcpNoDelay(SOCKET socket, bool flag);
+	static bool SetUpdateAcceptSocket(SOCKET socket, SOCKET listenSocket);
+
+	static bool Bind(SOCKET socket, SOCKADDR_IN netAddr);
+	static bool BindAnyAddress(SOCKET socket, uint16 port);
+	static bool Listen(SOCKET socket, int32 backlog = SOMAXCONN);
+	static void Close(SOCKET& socket);
+};
+
+template<typename T>
+static inline bool SetSockOpt(SOCKET socket, int32 level, int32 optName, T optVal)
+{
+	return SOCKET_ERROR != ::setsockopt(socket, level, optName, reinterpret_cast<char*>(&optVal), sizeof(T));
+}
